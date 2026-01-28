5fadefad7deca80b90a4aa7a10d6d9baa6515287
wu <wu@.com>
2026-01-28 17:36:39 +0800

    Logs-ryanzim/universalify

--- index.js
+++ index.js
@@ -1,50 +1,24 @@
-'use strict';
+'use strict'
 
-const dotenv = require('dotenv');
-const fs = require('fs');
-const MissingEnvVarsError = require('./MissingEnvVarsError.js');
-
-function difference (arrA, arrB) {
-    return arrA.filter(a => arrB.indexOf(a) < 0);
+exports.fromCallback = function (fn) {
+  return Object.defineProperty(function (...args) {
+    if (typeof args[args.length - 1] === 'function') fn.apply(this, args)
+    else {
+      return new Promise((resolve, reject) => {
+        fn.call(
+          this,
+          ...args,
+          (err, res) => (err != null) ? reject(err) : resolve(res)
+        )
+      })
+    }
+  }, 'name', { value: fn.name })
 }
 
-function compact (obj) {
-    const result = {};
-    Object.keys(obj).forEach(key => {
-        if (obj[key]) {
-            result[key] = obj[key];
-        }
-    });
-    return result;
+exports.fromPromise = function (fn) {
+  return Object.defineProperty(function (...args) {
+    const cb = args[args.length - 1]
+    if (typeof cb !== 'function') return fn.apply(this, args)
+    else fn.apply(this, args.slice(0, -1)).then(r => cb(null, r), cb)
+  }, 'name', { value: fn.name })
 }
-
-module.exports = {
-    config: function(options = {}) {
-        const dotenvResult = dotenv.config(options);
-        const example = options.example || options.sample || '.env.example';
-        const allowEmptyValues = options.allowEmptyValues || false;
-        const processEnv = allowEmptyValues ? process.env : compact(process.env);
-        const exampleVars = dotenv.parse(fs.readFileSync(example));
-        const missing = difference(Object.keys(exampleVars), Object.keys(processEnv));
-
-        if (missing.length > 0) {
-            throw new MissingEnvVarsError(allowEmptyValues, options.path || '.env', example, missing, dotenvResult.error);
-        }
-
-        // Key/value pairs defined in example file and resolved from environment
-        const required = Object.keys(exampleVars).reduce((acc, key) => {
-            acc[key] = process.env[key];
-            return acc;
-        }, {});
-        const error = dotenvResult.error ? { error: dotenvResult.error } : {};
-        const result = {
-            parsed: dotenvResult.error ? {} : dotenvResult.parsed,
-            required: required
-        };
-        return Object.assign(result, error);
-    },
-    parse: dotenv.parse,
-    MissingEnvVarsError: MissingEnvVarsError
-};
-
-module.exports.MissingEnvVarsError = MissingEnvVarsError;
