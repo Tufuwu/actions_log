1163f169232eef0da2c808626e8e560b63f3c890
wu <wu@.com>
2026-01-28 21:12:16 +0800

    Logs-earwig/mwparserfromhell

--- docs/usage.rst
+++ docs/usage.rst
@@ -1,26 +1,82 @@
-=====
 Usage
 =====
 
-To use Django Model Path Converter in a project with model `MyModel`::
+Normal usage is rather straightforward (where ``text`` is page text)::
 
-    from model_path_converter import register_model_converter
+    >>> import mwparserfromhell
+    >>> wikicode = mwparserfromhell.parse(text)
 
-    register_model_converter(MyModel)
+``wikicode`` is a :class:`mwparserfromhell.Wikicode <.Wikicode>` object, which
+acts like an ordinary ``str`` object with some extra methods. For example::
 
-Assuming you have a view `detail` that expects a `MyModel` instance as
-parameter named `obj`, you can then use it to define a URL pattern::
+    >>> text = "I has a template! {{foo|bar|baz|eggs=spam}} See it?"
+    >>> wikicode = mwparserfromhell.parse(text)
+    >>> print(wikicode)
+    I has a template! {{foo|bar|baz|eggs=spam}} See it?
+    >>> templates = wikicode.filter_templates()
+    >>> print(templates)
+    ['{{foo|bar|baz|eggs=spam}}']
+    >>> template = templates[0]
+    >>> print(template.name)
+    foo
+    >>> print(template.params)
+    ['bar', 'baz', 'eggs=spam']
+    >>> print(template.get(1).value)
+    bar
+    >>> print(template.get("eggs").value)
+    spam
 
-    path('<my_model:obj>/', views.detail, name='detail'),
+Since nodes can contain other nodes, getting nested templates is trivial::
 
-Here is a full example::
+    >>> text = "{{foo|{{bar}}={{baz|{{spam}}}}}}"
+    >>> mwparserfromhell.parse(text).filter_templates()
+    ['{{foo|{{bar}}={{baz|{{spam}}}}}}', '{{bar}}', '{{baz|{{spam}}}}', '{{spam}}']
 
-    from .models import MyModel
+You can also pass *recursive=False* to :meth:`.filter_templates` and explore
+templates manually. This is possible because nodes can contain additional
+:class:`.Wikicode` objects::
 
-    app_name = 'my_app'
-    urlpatterns = [
-        path('', views.IndexView.as_view(), name='index'),
-        path('<my_model:obj>/', views.detail, name='detail'),
-    ]
+    >>> code = mwparserfromhell.parse("{{foo|this {{includes a|template}}}}")
+    >>> print(code.filter_templates(recursive=False))
+    ['{{foo|this {{includes a|template}}}}']
+    >>> foo = code.filter_templates(recursive=False)[0]
+    >>> print(foo.get(1).value)
+    this {{includes a|template}}
+    >>> print(foo.get(1).value.filter_templates()[0])
+    {{includes a|template}}
+    >>> print(foo.get(1).value.filter_templates()[0].get(1).value)
+    template
 
-The `register_model_converter` function takes several optional arguments.
+Templates can be easily modified to add, remove, or alter params.
+:class:`.Wikicode` objects can be treated like lists, with
+:meth:`~.Wikicode.append`, :meth:`~.Wikicode.insert`,
+:meth:`~.Wikicode.remove`, :meth:`~.Wikicode.replace`, and more. They also have
+a :meth:`~.Wikicode.matches` method for comparing page or template names, which
+takes care of capitalization and whitespace::
+
+    >>> text = "{{cleanup}} '''Foo''' is a [[bar]]. {{uncategorized}}"
+    >>> code = mwparserfromhell.parse(text)
+    >>> for template in code.filter_templates():
+    ...     if template.name.matches("Cleanup") and not template.has("date"):
+    ...         template.add("date", "July 2012")
+    ...
+    >>> print(code)
+    {{cleanup|date=July 2012}} '''Foo''' is a [[bar]]. {{uncategorized}}
+    >>> code.replace("{{uncategorized}}", "{{bar-stub}}")
+    >>> print(code)
+    {{cleanup|date=July 2012}} '''Foo''' is a [[bar]]. {{bar-stub}}
+    >>> print(code.filter_templates())
+    ['{{cleanup|date=July 2012}}', '{{bar-stub}}']
+
+You can then convert ``code`` back into a regular :class:`str` object (for
+saving the page!) by calling :func:`str` on it::
+
+    >>> text = str(code)
+    >>> print(text)
+    {{cleanup|date=July 2012}} '''Foo''' is a [[bar]]. {{bar-stub}}
+    >>> text == code
+    True
+
+
+For more tips, check out :class:`Wikicode's full method list <.Wikicode>` and
+the :mod:`list of Nodes <.nodes>`.
--- setup.py
+++ setup.py
@@ -1,59 +1,115 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#! /usr/bin/env python
+#
+# Copyright (C) 2012-2021 Ben Kurtovic <ben.kurtovic@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-"""The setup script."""
+from glob import glob
+import os
+import sys
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup, Extension
+from setuptools.command.build_ext import build_ext
 
-with open("README.rst") as readme_file:
-    readme = readme_file.read()
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
+from mwparserfromhell import __version__
 
-requirements = [
-    "django>=2",
+with open("README.rst") as fp:
+    long_docs = fp.read()
+
+use_extension = True
+fallback = True
+
+# Allow env var WITHOUT_EXTENSION and args --with[out]-extension:
+
+env_var = os.environ.get("WITHOUT_EXTENSION")
+if "--without-extension" in sys.argv:
+    use_extension = False
+elif "--with-extension" in sys.argv:
+    fallback = False
+elif env_var is not None:
+    if env_var == "1":
+        use_extension = False
+    elif env_var == "0":
+        fallback = False
+
+# Remove the command line argument as it isn't understood by setuptools:
+
+sys.argv = [
+    arg for arg in sys.argv if arg not in ("--without-extension", "--with-extension")
 ]
 
-setup_requirements = [
-    "pytest-runner",
-]
 
-test_requirements = [
-    "pytest",
-]
+def build_ext_patched(self):
+    try:
+        build_ext_original(self)
+    except Exception as exc:
+        print("error: " + str(exc))
+        print("Falling back to pure Python mode.")
+        del self.extensions[:]
+
+
+if fallback:
+    build_ext.run, build_ext_original = build_ext_patched, build_ext.run
+
+# Project-specific part begins here:
+
+tokenizer = Extension(
+    "mwparserfromhell.parser._tokenizer",
+    sources=sorted(glob("src/mwparserfromhell/parser/ctokenizer/*.c")),
+    depends=sorted(glob("src/mwparserfromhell/parser/ctokenizer/*.h")),
+)
 
 setup(
-    author="Daniel Hepper",
-    author_email="daniel@consideratecode.com",
+    name="mwparserfromhell",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    ext_modules=[tokenizer] if use_extension else [],
+    setup_requires=["pytest-runner"]
+    if "test" in sys.argv or "pytest" in sys.argv
+    else [],
+    tests_require=["pytest"],
+    version=__version__,
+    python_requires=">= 3.6",
+    author="Ben Kurtovic",
+    author_email="ben.kurtovic@gmail.com",
+    url="https://github.com/earwig/mwparserfromhell",
+    description="MWParserFromHell is a parser for MediaWiki wikicode.",
+    long_description=long_docs,
+    download_url="https://github.com/earwig/mwparserfromhell/tarball/v{}".format(
+        __version__
+    ),
+    keywords="earwig mwparserfromhell wikipedia wiki mediawiki wikicode template parsing",
+    license="MIT License",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Framework :: Django",
+        "Topic :: Text Processing :: Markup",
     ],
-    description="The Django Model Path Converter package dynamically creates custom path converters for you models.",
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme + "\n\n" + history,
-    include_package_data=True,
-    keywords="django-model-path-converter",
-    name="django-model-path-converter",
-    packages=find_packages(include=["model_path_converter"]),
-    setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
-    url="https://github.com/dhepper/django-model-path-converter",
-    version="0.1.0",
-    zip_safe=False,
 )
