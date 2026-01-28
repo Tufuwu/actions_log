2b351eea5831181d29e12969bd65936c299d2f29
wu <wu@.com>
2026-01-29 00:12:37 +0800

    Logs-starou/simpleidml

--- setup.py
+++ setup.py
@@ -1,29 +1,54 @@
-#!/usr/bin/env python
-from setuptools import setup, find_packages  # This setup relies on setuptools since distutils is insufficient and badly hacked code
+import os
+from setuptools import setup
 
-version = '0.1.0'
-author = 'Pascal Wolf'
-author_email = 'wolf@physik.uni-bonn.de'
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+    README = f.read()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='irrad_spectroscopy',
-    version=version,
-    description='Gamma spectroscopy of irradiated sensors, chips and PCBs.',
-    url='https://github.com/SiLab-Bonn/irrad_spectroscopy',
-    license='MIT License',
-    long_description='',
-    author=author,
-    maintainer=author,
-    author_email=author_email,
-    maintainer_email=author_email,
-    packages=find_packages(),
-    setup_requires=['setuptools'],
-    install_requires=required,
-    include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
-    package_data={'': ['README.*', 'VERSION'], 'docs': ['*'], 'examples': ['*']},
-    keywords=['silicon', 'irradiation', 'radioactivity', 'gamma', 'spectroscopy'],
-    platforms='any'
+    name="SimpleIDML",
+    version="1.1.3",
+    license='BSD Licence',
+    author='Stanislas Guerra',
+    author_email='stanislas.guerra@gmail.com',
+    description='A library to manipulate Adobe(r) IDML(r) files.',
+    long_description=README,
+    url='https://github.com/Starou/SimpleIDML',
+    project_urls={
+        'Source Code': 'https://github.com/Starou/SimpleIDML',
+        'Issue Tracker': 'https://github.com/Starou/SimpleIDML/issues',
+    },
+    package_dir={'': 'src'},
+    install_requires=['lxml', 'suds-py3'],
+    packages=[
+        'simple_idml',
+        'simple_idml.indesign',
+    ],
+    package_data={
+        'simple_idml.indesign': [
+            'scripts/*.jsx',
+        ]
+    },
+    data_files=[],
+    scripts=[
+        'src/scripts/simpleidml_create_package_from_dir.py',
+        'src/scripts/simpleidml_indesign_save_as.py',
+        'src/scripts/simpleidml_indesign_close_all_documents.py',
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Printing',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
