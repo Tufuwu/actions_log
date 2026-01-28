898644fabcdc02afe246778665658d551f0d0bfe
wu <wu@.com>
2026-01-28 23:19:31 +0800

    Logs-winstonjs/winston-daily-rotate-file

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = require('./lib/i18n-zipcodes.js');
+'use strict';
+
+var winston = require('winston');
+var DailyRotateFile = require('./daily-rotate-file');
+
+winston.transports.DailyRotateFile = DailyRotateFile;
+module.exports = DailyRotateFile;
